c2a93a60f51a30aa31eeccc6f32a9c60fb693f5a
Cipi Bad <cipibad@gmail.com>
2018-04-17 21:51:30 +0300

    lab1.test1.step2.red

--- bowlingGameTest/GameTest.cs
+++ bowlingGameTest/GameTest.cs
@@ -11,7 +11,10 @@ namespace bowlingGameTest
         public void testGutterGame()
         {
             Game g = new Game();
-
+            for (int i=0; i<20; i++)
+            {
+                g.roll(0);
+            }
         }
     }
 }
