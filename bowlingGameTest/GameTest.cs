81f17645d34fe6a417116a02c8247c2e9179ce41
Cipi Bad <cipibad@gmail.com>
2018-04-17 21:44:26 +0300

    lab1.test1.step0.red

--- bowlingGameTest/GameTest.cs
+++ bowlingGameTest/GameTest.cs
@@ -1,9 +1,17 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using bowlingGame;
 
 namespace bowlingGameTest
 {
     [TestClass]
     public class gameTest
     {
+
+        [TestMethod]
+        public void testGutterGame()
+        {
+            Game g = new Game();
+
+        }
     }
 }
