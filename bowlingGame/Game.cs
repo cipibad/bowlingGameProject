473f59a2fde78b55732c76ef9e43df6259e09811
Cipi Bad <cipibad@gmail.com>
2018-04-19 09:30:42 +0300

    lab1.test3.step3.refactor

--- bowlingGame/Game.cs
+++ bowlingGame/Game.cs
@@ -1,18 +1,21 @@
 namespace bowlingGame{
     public class Game
     {
-        private int _score = 0;
         private int[] _rolls = new int[21];
         private int _currentRoll = 0;
 
         public void roll(int pins)
         {
-            _score += pins; //FIXME calculates score, while name is roll
-            _rolls[_currentRoll] += pins;
+            _rolls[_currentRoll++] = pins;
         }
         public int score()
-        {
-            return _score; //FIXME does not calculate score
+        {    
+            int score = 0;
+            for (int i = 0; i < _rolls.Length; i++)
+            {
+                score += _rolls[i]; 
+            }
+            return score;
         }
     }
 }
