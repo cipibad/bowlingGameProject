ed0e9c102ce246f1595736de25f4aa04bcbb609b
Cipi Bad <cipibad@gmail.com>
2018-04-19 08:57:28 +0300

    lab1.test3.step1.green

--- bowlingGame/Game.cs
+++ bowlingGame/Game.cs
@@ -4,11 +4,11 @@ namespace bowlingGame{
         private int _score = 0;
         public void roll(int pins)
         {
-            _score += pins;
+            _score += pins; //FIXME calculates score, while name is roll
         }
         public int score()
         {
-            return _score;
+            return _score; //FIXME does not calculate score
         }
     }
 }
