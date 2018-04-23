d9c5777603ba886ae578ba04a7aae5f6186361f0
Cipi Bad <cipibad@gmail.com>
2018-04-23 10:09:57 +0300

    lab1.test6.step0.red

--- bowlingGameTest/GameTest.cs
+++ bowlingGameTest/GameTest.cs
@@ -6,70 +6,58 @@ namespace bowlingGameTest
     [TestClass]
     public class gameTest
     {
-
-        private Game _game;
-
-        [TestInitialize()]
-        public void Initialize() 
-        {
-            _game = new Game();
-        }
-        private void rollMany(int n, int pins)
-        {
-            for (int i = 0; i < n; i++)
-            {
-                _game.roll(pins);
-            }
-        }
-
         [TestMethod]        
         public void testGutterGame()
         {
-            rollMany(20, 0);
-            Assert.AreEqual(0, _game.score());
+            Game g = new Game();
+            for (int i = 0; i < 20; i++)
+            {
+                g = g.roll(0);
+            }
+            Assert.AreEqual(0, g.score());
         }
 
-        [TestMethod]
-        public void testAllOnes()
-        {
-            rollMany(20, 1);
-            Assert.AreEqual(20, _game.score());
-        }
+        // [TestMethod]
+        // public void testAllOnes()
+        // {
+        //     rollMany(20, 1);
+        //     Assert.AreEqual(20, _game.score());
+        // }
 
-        [TestMethod]
-        public void testOneSpare()
-        {
-            rollSpare();
-            _game.roll(3);
-            rollMany(17,0);
-            Assert.AreEqual(16, _game.score());
-        }
+        // [TestMethod]
+        // public void testOneSpare()
+        // {
+        //     rollSpare();
+        //     _game.roll(3);
+        //     rollMany(17,0);
+        //     Assert.AreEqual(16, _game.score());
+        // }
 
-        [TestMethod]
-        public void testOneStrike()
-        {
-            rollStrike();
-            _game.roll(3);
-            _game.roll(4);
-            rollMany(16,0);
-            Assert.AreEqual(24, _game.score());
-        }
-        [TestMethod]
-        public void testPerfectGame()
-        {
-            rollMany(12,10);
-            Assert.AreEqual(300, _game.score());
-        }
+        // [TestMethod]
+        // public void testOneStrike()
+        // {
+        //     rollStrike();
+        //     _game.roll(3);
+        //     _game.roll(4);
+        //     rollMany(16,0);
+        //     Assert.AreEqual(24, _game.score());
+        // }
+        // [TestMethod]
+        // public void testPerfectGame()
+        // {
+        //     rollMany(12,10);
+        //     Assert.AreEqual(300, _game.score());
+        // }
 
-        private void rollStrike()
-        {
-            _game.roll(10);
-        }
+        // private void rollStrike()
+        // {
+        //     _game.roll(10);
+        // }
 
-        private void rollSpare()
-        {
-            _game.roll(5);
-            _game.roll(5);
-        }
+        // private void rollSpare()
+        // {
+        //     _game.roll(5);
+        //     _game.roll(5);
+        // }
     }
 }
